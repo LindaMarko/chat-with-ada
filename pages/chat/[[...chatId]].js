bafc5c46e59e85ae231b525e2e006b989c586fcd
Linda Marko <linda.marko@vaimo.com>
2023-09-13 17:53:49 +0200

    Stream the OpenAI response on the front end

--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -1,13 +1,31 @@
 import { ChatSidebar } from "components/ChatSidebar";
 import Head from "next/head";
+import { streamReader } from "openai-edge-stream";
 import { useState } from "react";
 
 export default function ChatPage() {
+  const [incomingMessage, setIncomingMessage] = useState("");
   const [messageText, setMessageText] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(messageText);
+    const response = await fetch(`/api/chat/sendMessage`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ message: messageText }),
+    });
+    const data = response.body;
+    if (!data) {
+      return;
+    }
+    const reader = data.getReader();
+    await streamReader(reader, (message) => {
+      console.log(message);
+      setIncomingMessage((s) => `${s}${message.content}`);
+    });
   };
   return (
     <>
@@ -17,7 +35,7 @@ export default function ChatPage() {
       <div className="grid h-screen grid-cols-[260px_1fr]">
         <ChatSidebar />
         <div className="flex flex-col bg-gray-700">
-          <div className="flex-1"></div>
+          <div className="flex-1 text-white">{incomingMessage}</div>
           <footer className="bg-gray-800 p-10">
             <form onSubmit={handleSubmit}>
               <fieldset className="flex gap-2">
